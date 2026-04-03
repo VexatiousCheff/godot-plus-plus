f1ec46d13d2cddda87b00b798f0ba79d10fc7d3f
VexatiousCheff <58625570+VexatiousCheff@users.noreply.github.com>
2026-04-03 20:29:58 +1000

    fix pitching issues or something

--- src/opuslib.cpp
+++ src/opuslib.cpp
@@ -21,18 +21,18 @@ godot::String OpusLib::str_error(int err) {
 godot::PackedByteArray OpusLib::convert_signed_pcm(const godot::PackedVector2Array &pcm) {
 	godot::PackedByteArray arr;
 
-	arr.resize(pcm.size() * 2 * sizeof(int16_t)); // 2 bytes per sample (2 channels * 2 bytes per sample)
+	arr.resize(pcm.size() * sizeof(int16_t)); // 2 bytes per sample (2 channels * 2 bytes per sample)
 
 	uint8_t *data = arr.ptrw();
 
 	for (size_t i = 0; i < pcm.size(); i++) {
 		int16_t left_sample = static_cast<int16_t>(pcm[i].x * 32767.0f);
-		int16_t right_sample = static_cast<int16_t>(pcm[i].y * 32767.0f);
+		//int16_t right_sample = static_cast<int16_t>(pcm[i].y * 32767.0f);
 
 		memcpy(data, &left_sample, sizeof(int16_t));
-		memcpy(data + sizeof(int16_t), &right_sample, sizeof(int16_t));
+		//memcpy(data + sizeof(int16_t), &right_sample, sizeof(int16_t));
 
-		data += sizeof(int16_t) * 2; // Move to the next sample (2 channels)
+		data += sizeof(int16_t); // Move to the next sample (2 channels)
 	}
 
 	return arr;
@@ -41,22 +41,20 @@ godot::PackedByteArray OpusLib::convert_signed_pcm(const godot::PackedVector2Arr
 godot::PackedVector2Array OpusLib::convert_unsigned_pcm(const godot::PackedByteArray &pcm) {
 	godot::PackedVector2Array arr;
 
-	size_t num_samples = pcm.size() / (2 * sizeof(int16_t)); // 2 bytes per sample (2 channels * 2 bytes per sample)
+	size_t num_samples = pcm.size() / (sizeof(int16_t)); // 2 bytes per sample (2 channels * 2 bytes per sample)
 	arr.resize(num_samples);
 
 	const uint8_t *data = pcm.ptr();
 
 	for (size_t i = 0; i < num_samples; i++) {
 		int16_t left_sample;
-		int16_t right_sample;
 
         memcpy(&left_sample, data, sizeof(int16_t));
-        memcpy(&right_sample, data + sizeof(int16_t), sizeof(int16_t));
         
 		arr[i].x = static_cast<float>(left_sample) / 32767.0f;
-		arr[i].y = static_cast<float>(right_sample) / 32767.0f;
+		arr[i].y = arr[i].x;
 
-		data += sizeof(int16_t) * 2; // Move to the next sample (2 channels)
+		data += sizeof(int16_t); // Move to the next sample (2 channels)
 	}
 
 	return arr;
